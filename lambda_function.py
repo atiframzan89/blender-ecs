7827fda4067350b0e40e6923fe23ebeb892e30e9
Ubuntu <ubuntu@ip-172-31-88-146.ec2.internal>
2023-11-09 08:43:39 +0000

    changing lambda_function.py for calling a function

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,10 @@
 import json
 import os
 
-def lambda_handler(event, context):
+def lambda_handler():
     os.system(f"./blender -b -P script.py")
     #os.system(f"./blender -b -P script.py -- {event.get('width', 0)} {event.get('height', 0)}")
     os.system(f"ls -ltrh")
     os.system(f"pwd")
     return 'Just print me'
-#lambda_handler()
+lambda_handler()
